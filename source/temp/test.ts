ca39806f625bc4b78e073751537f3514d178b8c7
gracexwho <grace.hu101@gmail.com>
2020-12-16 23:06:54 -0500

    Refactor notebook for analysis and delete /source/temp/

--- source/temp/test.ts
+++
@@ -1,5 +0,0 @@
-function displayTotalPerPerson(person: string, total: number) {
-    let message: string = "Total for " + person + " is " + total;
-    // @ts-ignore
-    document.getElementById("totalMessage").innerText = message;
-}
