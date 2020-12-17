d03a3b7d98f75a81173fabfee63468f39ba9fe40
gracexwho <grace.hu101@gmail.com>
2020-12-16 22:49:35 -0500

    Remove dist folder from git repo

--- dist/test.d.ts
+++
@@ -1,1 +0,0 @@
-declare function displayTotalPerPerson(person: string, total: number): void;
--- dist/test.js
+++
@@ -1,6 +0,0 @@
-function displayTotalPerPerson(person, total) {
-    var message = "Total for " + person + " is " + total;
-    // @ts-ignore
-    document.getElementById("totalMessage").innerText = message;
-}
-//# sourceMappingURL=test.js.map
